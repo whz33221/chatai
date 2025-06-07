d2008a3c8f85df1396801b560b4b4946fb8e759e
wuhongzhang <812494537@qq.com>
2025-06-07 16:03:36 +0800

    实现最简易的多轮对话功能

--- src/main/java/com/example/chatai/service/ChatService.java
+++ src/main/java/com/example/chatai/service/ChatService.java
@@ -4,10 +4,17 @@ import jakarta.annotation.Resource;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.DefaultChatClientBuilder;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.stereotype.Service;
+
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY;
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_RETRIEVE_SIZE_KEY;
 
 /**
  * @author wuhongzhang@vhsoft.com.cn
@@ -19,8 +26,36 @@ public class ChatService {
     @Resource
     private ChatModel dashScopeChatModel;
 
+    private final ChatClient chatClient;
+
+    public ChatService(ChatClient.Builder builder) {
+        InMemoryChatMemory chatMemory = new InMemoryChatMemory();
+        this.chatClient = builder
+//                //顾问
+                .defaultAdvisors(new MessageChatMemoryAdvisor(chatMemory))
+//                //配置
+//                .defaultOptions()
+                //系统提示词
+                .defaultSystem("你是档案管理员").build();
+    }
+
     public String chat(String input){
         ChatResponse call = dashScopeChatModel.call(new Prompt(input));
         return call.getResult().getOutput().getText();
     }
+
+    public String chatLoop(String input,String chatId){
+        ChatResponse chatResponse = chatClient
+                .prompt()
+                .user(input)
+                .advisors(spec -> spec
+                        .param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
+                        .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
+                .call()
+                .chatResponse();
+        return chatResponse.getResult().getOutput().getText();
+    }
+
+
+
 }
--- src/test/java/com/example/chatai/service/ChatServiceTest.java
+++ src/test/java/com/example/chatai/service/ChatServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.chatai.service;
 
+import cn.hutool.core.lang.UUID;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,6 +21,24 @@ class ChatServiceTest {
     public void testchat(){
         String res = chatService.chat("你是谁");
         System.out.println(res);
+    }
+
+
+    @Test
+    public void testchatLoop(){
+        //uuid 作为 chat id;
+        String chatid = "c6ad9a7c511542599bd2ad9adb4db011";
+        String res = chatService.chatLoop("你是谁",chatid);
+        System.out.println(res);
+         res = chatService.chatLoop("你知道国家管理办法吗",chatid);
+        System.out.println(res);
+         res = chatService.chatLoop("你总共回答了我几个问题，包括现在这一次回答",chatid);
+        System.out.println(res);
+    }
+
+    public static void main(String[] args) {
+        String string = UUID.fastUUID().toString(true);
+        System.out.println(string);
     }
 
 }
